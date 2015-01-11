ce74a7587d81942dae3b558d3a25d84bed3d22c2
Marylandeny-Chu <chu849238686@foxmail.com>
2015-01-11 13:32:56 +0800

    First commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Algorithm-training
+
+这个项目是假期做算法培训时的slides和相关示例程序
---
+++ src/Recursion/1-1.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int f[101];
+    int i;
+    int n;
+    scanf("%d", &n);
+    /*
+
+    */
+    printf("%d\n", f[n]);
+    return 0;
+}
