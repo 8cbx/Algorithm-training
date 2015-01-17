626ac185f8f63025ee697f19b3536cc1a98a9580
Marylandeny-Chu <chu849238686@foxmail.com>
2015-01-17 22:10:29 +0800

    Add some files

---
+++ src/README.md
@@ -0,0 +1,3 @@
+# Algorithm-training
+## 示例程序  
+* Recursion 递推与递归  
---
+++ src/Recursion/0-2.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int f(int n)
+{
+    /*
+
+    */
+}
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    printf("%d\n",f(n));
+    return 0;
+}
